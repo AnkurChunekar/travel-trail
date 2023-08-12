ace8c8b5fcd878574293a3cd23c45e0dd5b4fd38
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2023-08-12 23:38:33 +0530

    feat: add additional crud ops in tour route
    Added methods such as getUniqueTour, updateTour, and deleteTour. Also used correct status codes and refactored old tour controllers

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,8 +4,10 @@ exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find({});
     res.json({
-      message: "Hello bro",
-      tours
+      data: {
+        tours
+      },
+      results: tours.length
     });
   } catch (error) {
     res.status(400).json({
@@ -17,20 +19,84 @@ exports.getAllTours = async (req, res) => {
 exports.addNewTour = async (req, res) => {
   try {
     const { name, rating, price } = req.body;
-    const tour = new Tour({
+    const result = await Tour.create({
       name,
       rating,
       price
     });
 
+    /*  
+
+    Alternative way
+    const tour = new Tour({...data_here});
     const result = await tour.save();
-    res.json({
+
+    */
+
+    res.status(201).json({
       message: "Success, added new doc",
-      result
+      data: {
+        tour: result
+      }
     });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({
+      message: error.message
+    });
+  }
+};
+
+exports.getUniqueTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // alternate
+    // Tour.findOne({ _id: req.params.id })
+    res.json({
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: "Something went wrong"
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.json({
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: "Something went wrong",
+      message: error.message
+    });
+  }
+};
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: "Something went wrong",
       message: error.message
     });
   }
