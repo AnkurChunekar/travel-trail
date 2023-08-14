55405a8e3c4384faeaf7e00d7ec13701fa1c23e0
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2023-08-14 22:33:01 +0530

    refactor: move apiFeatures class to new module

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,5 @@
 const Tour = require("../models/tourModel");
-
-const EXCLUDE_QUERY_PARAMS = ["limit", "sort", "page", "projection"];
-const QUERY_OPERATORS_REGEX = /\b(gte|gt|lte|lt)\b/g;
+const APIQueryFeatures = require("../utils/apiQueryFeatures");
 
 exports.getTop5AffordableQuery = async (req, res, next) => {
   req.query = {
@@ -12,55 +10,6 @@ exports.getTop5AffordableQuery = async (req, res, next) => {
   };
   next();
 };
-
-class APIQueryFeatures {
-  constructor(query, queryObj) {
-    this.query = query;
-    this.queryObj = queryObj;
-  }
-
-  sanitizeQueryObj() {
-    let result = { ...this.queryObj };
-
-    EXCLUDE_QUERY_PARAMS.forEach((item) => delete result[item]);
-
-    // 1. QUERYING
-    result = JSON.parse(
-      JSON.stringify(result).replace(
-        QUERY_OPERATORS_REGEX,
-        (matched) => `$${matched}`
-      )
-    );
-
-    this.query.find(result);
-    return this;
-  }
-
-  sort() {
-    if (this.queryObj.sort) {
-      this.query.sort(this.queryObj.sort.replaceAll(",", " "));
-    } else this.query.sort("-createdAt");
-    return this;
-  }
-
-  project() {
-    if (this.queryObj.projection) {
-      this.query.select(this.queryObj.projection.split(","));
-    }
-    return this;
-  }
-
-  paginate() {
-    // by default we will keep 100 as limit, why? think if there are million records then?
-    let page = Number(this.queryObj.page) || 1;
-    if (page < 1) page = 1;
-    const limit = Math.abs(Number(this.queryObj.limit)) || 100;
-    const skip = limit * (page - 1);
-
-    this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 exports.getAllTours = async (req, res) => {
   try {
