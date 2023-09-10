b9958c874a79ff8ccc59420bb54223a4df872944
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2023-09-10 16:08:08 +0530

    feat: handle global unhandled exceptions and rejections

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ const app = require("./app");
 
 const PORT = 3000;
 
+process.on("uncaughtException", (err) => {
+  console.error("Global error", err.name, err.message);
+  console.error("Unhandled exception, Shutting down 🔥");
+  process.exit(1);
+});
+
 initializeDbConnection();
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("Global error", err.name, err.message);
+  console.error("Unhandled rejection, Shutting down 🔥");
+  server.close(() => {
+    process.exit(1);
+  });
 });
