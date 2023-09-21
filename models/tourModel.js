46df12d523fcdf4ddab76a844c58370771195755
AnkurChunekar <ankurchunekar@gmail.com>
2023-09-21 13:41:22 +0530

    fix: tour model code

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,39 +1,146 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Please provide your email"],
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate: {
-      // eslint-disable-next-line object-shorthand
-      validator: function validateEmail(email) {
-        // Regular expression to match a valid email address
-        const regex =
-          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// Docs for the features used here.
+// Validation: https://mongoosejs.com/docs/validation.html
+// virtuals: https://mongoosejs.com/docs/tutorials/virtuals.html
+// middlewares: https://mongoosejs.com/docs/middleware.html
 
-        // Return true if the email string matches the regular expression
-        // if false then we get walidation error.
-        return regex.test(email);
-      },
-      message: "Please enter a valid email."
+const tourSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+      maxLength: [40, "Name can be maximum of 40 characters"],
+      minLength: [10, "Name must be atleast of 10 characters"]
+      // enum: ["Coffee", "Tea"] // if we want the string to be of fixed type
+      // how to specify message ? check difficulty
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"],
+      unique: false
+    },
+    maxGroupSize: {
+      type: String,
+      required: [true, "A tour must have a maxGroupSize"]
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "difficulty can only be 'easy', 'medium', 'difficult'"
+      }
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: 1,
+      max: 5
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, "A tour must have a price"],
+      min: [500, "Too cheap to be trusted, min should be 500."],
+      max: [10000, "Too costly to be afforded, max can be 10000"]
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        // eslint-disable-next-line object-shorthand
+        validator: function priceDiscountValidator(value) {
+          return value < this.price;
+        },
+        message: "priceDiscount (i.e {VALUE}) should be less than price!"
+      }
+    },
+    summary: {
+      type: String,
+      trim: true
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    startDates: [Date],
+    isPrivateTour: {
+      type: Boolean,
+      default: false
     }
   },
-  name: { type: String, required: [true, "Please provide your name"] },
-  photo: String,
-  password: {
-    type: String,
-    required: [true, "Please provide your password"],
-    minLength: [8, "password must be atleast of 8 characters"]
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, "Please confirm your email"]
-  }
+  { toJSON: { virtuals: true } },
+  { toObject: { virtuals: true } }
+);
+
+tourSchema.virtual("durationInWeek").get(function getDurationInWeeks() {
+  return (this.duration / 7).toFixed(2);
+});
+// obviously virtual properties cannot be used in queries, cause these are not present on documents in mongoDB
+
+// Just like express we mongoose also has middleware.
+
+// DOCUMENT MIDDLEWARE
+tourSchema.pre("save", function tourSchemaPre(next) {
+  // console.log(this); // prints this current document to be saved.
+  this.slug = this.name.toLowerCase().replaceAll(" ", "-");
+  next();
 });
 
-const User = new mongoose.model("User", schema);
+// just to show that multiple middlewares can be added.
+// tourSchema.pre("save", function tourSchemaPreAgain(next) {
+//   console.log(`Just printing name: ${this.name} b4 saving the doc.`);
+//   next();
+// });
 
-module.exports = User;
+// tourSchema.post("save", (doc, next) => {
+//   console.log("From mongoose document post middleware", { doc });
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+// have used a regex here match any string that starts with find such as findOne, find, etc
+
+// tourSchema.pre(/^find/, function tourSchemaFindPre(next) {
+//   // `this` here is a query object not the document
+//   // chaining the find method
+//   this.find({ isPrivateTour: { $ne: true } });
+
+//   this.start = Date.now();
+//   next();
+// });
+
+// tourSchema.post(/^find/, function tourSchemaFindPre(docs, next) {
+//   // `this` here is a query object not the document
+//   // chaining the find method
+//   console.log(`Query took ${Date.now() - this.start} ms to run.`);
+//   next();
+// });
+
+// AGGREGATION MIDDLEWARE
+
+// tourSchema.pre("aggregate", function tourSchemaFindPre(next) {
+//   // `this` here is a current aggregation object not the document
+//   // console.log(this._pipeline); // prints the pipeline array
+//   this._pipeline.unshift({ $match: { isPrivateTour: { $ne: true } } });
+//   next();
+// });
+
+const Tour = new mongoose.model("Tour", tourSchema);
+
+module.exports = Tour;
