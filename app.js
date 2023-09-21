f55300623be24d0597fad86bef949c562907430b
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2023-09-21 12:20:28 +0530

    feat: update app.js to use userRouter middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 
 const tourRouter = require("./routes/tourRoutes");
 const CustomError = require("./utils/customError");
-// const userRouter = require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const errorHandler = require("./controllers/errorController");
 
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
-// app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
   next(
