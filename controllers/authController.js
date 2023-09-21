d9a8ece327c1634568da9e7f5d5348bb20fa7159
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2023-09-21 12:18:12 +0530

    feat: add auth controllers

---
+++ controllers/authController.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/userModel");
+const catchAsyncError = require("../utils/catchAsyncError");
+const CustomError = require("../utils/customError");
+
+const getToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d"
+  });
+
+exports.signup = catchAsyncError(async (req, res, next) => {
+  const { email, name, photo, password, passwordConfirm } = req.body;
+
+  if (password !== passwordConfirm)
+    return next(
+      new CustomError("Password and confirm password should be same", 400)
+    );
+
+  const user = await User.create({ email, name, photo, password });
+  // eslint-disable-next-line no-underscore-dangle
+  const token = getToken(user._id);
+
+  res.status(201).json({
+    status: "success",
+    token,
+    data: { name: user.name, email: user.email, photo: user.photo }
+  });
+});
+
+exports.login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  const genericErr = new CustomError("Email or Password is incorrect", 401);
+
+  // if email or password not sent in body
+  if (!email || !password) return next(genericErr);
+
+  const user = await User.findOne({ email }).select("_id email password");
+
+  // if user is not present in DB
+  if (!user) return next(genericErr);
+
+  // check if valid password
+  const match = await user.isPasswordCorrect(password, user.password);
+
+  if (!match) return next(genericErr);
+
+  // eslint-disable-next-line no-underscore-dangle
+  const token = getToken(user._id);
+
+  res.status(200).json({ status: "success", token });
+});
