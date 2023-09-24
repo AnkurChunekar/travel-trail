5791095bfdd52b27269424adbd7e36d89283afdd
AnkurChunekar <ankurchunekar@gmail.com>
2023-09-24 15:41:19 +0530

    feat: implement rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const { rateLimit } = require("express-rate-limit");
 
 const tourRouter = require("./routes/tourRoutes");
 const CustomError = require("./utils/customError");
@@ -9,11 +10,21 @@ const errorHandler = require("./controllers/errorController");
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (60 minutes),
+  message: "Too many requests, please try again after an hour.",
+  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
+
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use("/api", limiter);
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
