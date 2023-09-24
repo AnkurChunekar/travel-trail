ea1dc24300732a69791a0d7f07d6c01b18123e80
AnkurChunekar <ankurchunekar@gmail.com>
2023-09-24 15:26:20 +0530

    feat: send jwt in cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,17 @@ const sendResWithToken = ({ res, user, jsonData = {}, statusCode = 200 }) => {
     expiresIn: process.env.JWT_EXPIRY
   });
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + Number(process.env.JWT_COOKIE_EXPIRY) * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
   res.status(statusCode).json({ ...jsonData, token, status: "success" });
 };
 
