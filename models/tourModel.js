dd66b36e4e7fe5fdeb540367567cf8aff596446e
AnkurChunekar <ankurchunekar@gmail.com>
2023-10-08 22:52:53 +0530

    feat: add geolocation data

--- models/tourModel.js
+++ models/tourModel.js
@@ -82,7 +82,30 @@ const tourSchema = mongoose.Schema(
     isPrivateTour: {
       type: Boolean,
       default: false
-    }
+    },
+    startLocation: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"]
+      },
+      coordinates: [Number],
+      address: String,
+      description: String
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"]
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number
+      }
+    ]
   },
   { toJSON: { virtuals: true } },
   { toObject: { virtuals: true } }
