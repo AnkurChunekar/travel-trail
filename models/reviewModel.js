fd55afd67b1aa647df16c764373153379f02c68a
AnkurChunekar <ankurchunekar@gmail.com>
2023-10-24 22:37:09 +0530

    feat: add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "A Review is required"]
+    },
+    rating: {
+      type: Number,
+      required: [true, "A Rating is required"],
+      max: 5,
+      min: 1
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"]
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"]
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    }
+  },
+  { toJSON: { virtuals: true } },
+  { toObject: { virtuals: true } }
+);
+
+const Review = new mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
