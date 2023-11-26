803d504e991aefa6bffd957a2cb311c99e86e34d
AnkurChunekar <ankurchunekar@gmail.com>
2023-11-26 21:52:07 +0530

    feat: build the tours overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,13 @@
-exports.getOverview = (_, res) => {
-  res.status(200).render("tour", { title: "All Tours" });
-};
+const Tour = require("../models/tourModel");
+const catchAsyncError = require("../utils/catchAsyncError");
+
+exports.getOverview = catchAsyncError(async (_, res) => {
+  // 1. Get all tours
+  const tours = await Tour.find();
+
+  res.status(200).render("overview", { title: "All Tours", tours });
+});
+
 exports.getTour = (_, res) => {
-  res.status(200).render("overview", { title: "The Forest Hiker" });
+  res.status(200).render("tour", { title: "The Forest Hiker" });
 };
