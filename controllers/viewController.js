4f60d1304ab8c6a3613683ba95c9759d381dd15d
AnkurChunekar <ankurchunekar@gmail.com>
2023-11-27 21:50:48 +0530

    feat: single tour template except location UI

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -8,6 +8,12 @@ exports.getOverview = catchAsyncError(async (_, res) => {
   res.status(200).render("overview", { title: "All Tours", tours });
 });
 
-exports.getTour = (_, res) => {
-  res.status(200).render("tour", { title: "The Forest Hiker" });
-};
+exports.getTour = catchAsyncError(async (req, res) => {
+  const { slug } = req.params;
+  const tour = await Tour.findOne({ slug }).populate({
+    path: "reviews",
+    fields: "review rating user"
+  });
+
+  res.status(200).render("tour", { title: "The Forest Hiker", tour });
+});
