a9d08016e51f0a72b184b4c4882c00b09567953c
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-03 12:34:33 +0530

    refactor: login js to support esmodule for webpack

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,21 @@
 /* eslint-disable */
-const form = document.querySelector("#form");
+import axios from "axios";
 
-if (form) {
-  const triggerLogin = async (email, password) => {
-    try {
-      const res = await axios({
-        method: "post",
-        url: "/api/v1/users/login",
-        data: { email, password }
-      });
+export const userLogin = async (email, password) => {
+  try {
+    const res = await axios({
+      method: "post",
+      url: "/api/v1/users/login",
+      data: { email, password }
+    });
 
-      alert(res.data.message);
-      // navigate the user to home route
-      location.assign("/");
-    } catch (error) {
-      alert(
-        error.response.data.message ||
-          "Something went wrong, please try again later!"
-      );
-    }
-  };
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    // construct a FormData object, which fires the formdata event
-    const formData = new FormData(form);
-    // formdata gets modified by the formdata event
-    triggerLogin(formData.get("email"), formData.get("password"));
-  });
-}
+    alert(res.data.message);
+    // navigate the user to home route
+    location.assign("/");
+  } catch (error) {
+    alert(
+      error.response.data.message ||
+        "Something went wrong, please try again later!"
+    );
+  }
+};
