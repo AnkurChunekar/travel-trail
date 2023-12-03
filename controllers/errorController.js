82d7fd2a0d072c5bedd040b7586a97edd534d7cd
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-03 19:26:49 +0530

    feat: show error UI on client if error occurs

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,29 +2,55 @@
 
 const CustomError = require("../utils/customError");
 
-const handleErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    stack: err.stack,
-    error: err
-  });
-};
-
-const handleErrorProd = (err, res) => {
-  if (err.isOperational) {
+const handleErrorDev = (err, req, res) => {
+  if (req.originalUrl.startsWith("/api")) {
+    // API ERROR HANDLING
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
+      message: err.message,
+      stack: err.stack,
+      error: err
     });
   } else {
+    // VIEW ERROR HANDLING
+    res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message
+    });
+  }
+};
+
+const handleErrorProd = (err, req, res) => {
+  if (req.originalUrl.startsWith("/api")) {
+    // API ERROR HANDLING
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    }
+
     console.error("Error ", err);
-    res.status(500).json({
+    return res.status(500).json({
       message: "Something went very wrong!",
       status: "error",
       statusCode: 500
     });
   }
+
+  // VIEW ERROR HANDLING
+  if (err.isOperational) {
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong!",
+      msg: err.message
+    });
+  }
+
+  console.error("Error ", err);
+  res.status(err.statusCode).render("error", {
+    title: "Something went wrong!",
+    msg: "Please try again later."
+  });
 };
 
 const handleKnownErrors = (error) => {
@@ -54,7 +80,7 @@ const handleKnownErrors = (error) => {
   return error;
 };
 
-module.exports = (err, _, res, next) => {
+module.exports = (err, req, res, next) => {
   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
@@ -63,8 +89,8 @@ module.exports = (err, _, res, next) => {
   error = handleKnownErrors(error);
 
   if (process.env.NODE_ENV === "production") {
-    handleErrorProd(error, res);
-  } else handleErrorDev(error, res);
+    handleErrorProd(error, req, res);
+  } else handleErrorDev(error, req, res);
 
   next();
 };
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require("../models/tourModel");
 const catchAsyncError = require("../utils/catchAsyncError");
+const CustomError = require("../utils/customError");
 
 exports.getOverview = catchAsyncError(async (_, res) => {
   // 1. Get all tours
@@ -8,12 +9,17 @@ exports.getOverview = catchAsyncError(async (_, res) => {
   res.status(200).render("overview", { title: "All Tours", tours });
 });
 
-exports.getTour = catchAsyncError(async (req, res) => {
+exports.getTour = catchAsyncError(async (req, res, next) => {
   const { slug } = req.params;
   const tour = await Tour.findOne({ slug }).populate({
     path: "reviews",
     fields: "review rating user"
   });
+
+  if (!tour)
+    return next(
+      new CustomError("A tour with the provided name does not exist", 404)
+    );
 
   res.status(200).render("tour", { title: `${tour.name} Tour`, tour });
 });
