2401519ed6af7dde085a78a87f5f918385f2fbf4
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-03 12:35:30 +0530

    feat: bundle all js file and import only single bundle

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,22 @@
-console.log("Hello world");
+/* eslint-disable */
+import { userLogin } from "./login";
+import { enableMap } from "./mapbox";
+
+// DOM ELEMENTS
+const form = document.querySelector("#form");
+const mapEl = document.getElementById("map");
+
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // construct a FormData object, which fires the formdata event
+    const formData = new FormData(form);
+    // formdata gets modified by the formdata event
+    userLogin(formData.get("email"), formData.get("password"));
+  });
+}
+
+if (mapEl) {
+  enableMap(mapEl);
+}
