b3c35db5427d21d652222eebcdd981606a577e0d
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-03 12:34:57 +0530

    refactor: mapbox.js to support webpack esmodules

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,27 +1,31 @@
 /* eslint-disable */
-const locations = JSON.parse(document.getElementById("map").dataset.locations);
+import L from "leaflet";
 
-var map = L.map("map", { zoomControl: false }); //to disable + - zoom
-// var map = L.map('map', { zoomControl: false }).setView([31.111745, -118.113491], );
+export const enableMap = (mapEl) => {
+  const locations = JSON.parse(mapEl.dataset.locations);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  crossOrigin: ""
-}).addTo(map);
+  var map = L.map("map", { zoomControl: false }); //to disable + - zoom
+  // var map = L.map('map', { zoomControl: false }).setView([31.111745, -118.113491], );
 
-const points = [];
-locations.forEach((loc) => {
-  points.push([loc.coordinates[1], loc.coordinates[0]]);
-  L.marker([loc.coordinates[1], loc.coordinates[0]])
-    .addTo(map)
-    .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
-      autoClose: false
-    })
-    .openPopup();
-});
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    crossOrigin: ""
+  }).addTo(map);
 
-const bounds = L.latLngBounds(points).pad(0.5);
-map.fitBounds(bounds);
+  const points = [];
+  locations.forEach((loc) => {
+    points.push([loc.coordinates[1], loc.coordinates[0]]);
+    L.marker([loc.coordinates[1], loc.coordinates[0]])
+      .addTo(map)
+      .bindPopup(`<p>Day ${loc.day}: ${loc.description}</p>`, {
+        autoClose: false
+      })
+      .openPopup();
+  });
 
-map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
+  const bounds = L.latLngBounds(points).pad(0.5);
+  map.fitBounds(bounds);
+
+  map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
+};
