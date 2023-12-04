0ef3a947710d3fec59863e8a669d088718ded08f
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-04 19:15:04 +0530

    refactor: select login form using new classname

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,16 @@ import { userLogin, userLogout } from "./login";
 import { enableMap } from "./mapbox";
 
 // DOM ELEMENTS
-const form = document.querySelector("#form");
+const loginForm = document.querySelector(".form.form--login");
 const mapEl = document.querySelector("#map");
 const logoutEl = document.querySelector("#logout");
 
-if (form) {
-  form.addEventListener("submit", (e) => {
+if (loginForm) {
+  loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     // construct a FormData object, which fires the formdata event
-    const formData = new FormData(form);
+    const formData = new FormData(loginForm);
     // formdata gets modified by the formdata event
     userLogin(formData.get("email"), formData.get("password"));
   });
