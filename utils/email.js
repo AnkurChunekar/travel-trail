232321c9737c94f865db2b2a7dd856a1d59ab704
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-13 21:05:16 +0530

    feat: update email util with new Email class

--- utils/email.js
+++ utils/email.js
@@ -1,30 +1,85 @@
 const nodemailer = require("nodemailer");
+const pug = require("pug");
+const path = require("path");
+const { htmlToText } = require("html-to-text");
 
-// Send the email
-const sendEmail = (options) => {
-  const { EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_HOST, EMAIL_PORT } =
-    process.env;
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.url = url;
+    this.from = `${process.env.EMAIL_FROM_NAME} <${process.env.EMAIL_FROM}>`;
+    [this.firstName, this.lastName] = user.name.split(" ");
+  }
 
-  // Create a transporter object
-  const transporter = nodemailer.createTransport({
-    host: EMAIL_HOST,
-    port: EMAIL_PORT,
-    auth: {
-      user: EMAIL_USERNAME,
-      pass: EMAIL_PASSWORD
+  static createNewTransport() {
+    if (process.env.NODE_ENV === "production") {
+      return "create a production transporter";
     }
-  });
 
-  // Compose the email message
-  const mailOptions = {
-    from: "John Doe <johndoe@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html: "<h1>Some html can be passed here</h1>"
-  };
+    const { EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_HOST, EMAIL_PORT } =
+      process.env;
 
-  return transporter.sendMail(mailOptions);
+    return nodemailer.createTransport({
+      host: EMAIL_HOST,
+      port: EMAIL_PORT,
+      auth: {
+        user: EMAIL_USERNAME,
+        pass: EMAIL_PASSWORD
+      }
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      path.join(__dirname, `/views/emails/${template}.pug`),
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html)
+    };
+
+    await this.createNewTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to travel trail family");
+  }
 };
 
-module.exports = sendEmail;
+// Old send mail code
+// const sendEmail = (options) => {
+//   const { EMAIL_USERNAME, EMAIL_PASSWORD, EMAIL_HOST, EMAIL_PORT } =
+//     process.env;
+
+//   // Create a transporter object
+//   const transporter = nodemailer.createTransport({
+//     host: EMAIL_HOST,
+//     port: EMAIL_PORT,
+//     auth: {
+//       user: EMAIL_USERNAME,
+//       pass: EMAIL_PASSWORD
+//     }
+//   });
+
+//   // Compose the email message
+//   const mailOptions = {
+//     from: "John Doe <johndoe@gmail.com>",
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message
+//     // html: "<h1>Some html can be passed here</h1>"
+//   };
+
+//   return transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
