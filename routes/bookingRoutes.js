eaf3674224948403d5b0d755cc418a5630c64d53
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-19 13:00:23 +0530

    feat: add booking routes

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,14 @@
+// external
+const express = require("express");
+
+// internal
+const bookingController = require("../controllers/bookingController");
+const authController = require("../controllers/authController");
+
+const router = express.Router();
+
+router
+  .route("/checkout-session/:tourId")
+  .get(authController.protect, bookingController.getCheckoutSession);
+
+module.exports = router;
