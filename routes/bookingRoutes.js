4ef470324dd4d58cd4e4af06c7ca92e0f1bf36ed
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-19 21:09:09 +0530

    feat: consume additional booking routes

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,19 +4,28 @@ const express = require("express");
 // internal
 const bookingController = require("../controllers/bookingController");
 const authController = require("../controllers/authController");
+const { ROLES } = require("../constants");
 
 const router = express.Router();
 
-router.get(
-  "/checkout-session/:tourId",
-  authController.protect,
-  bookingController.getCheckoutSession
-);
+router.use(authController.protect);
 
-router.post(
-  "/verify-payment",
-  authController.protect,
-  bookingController.verifyPaymentAndCreateBooking
-);
+router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
+router.post("/verify-payment", bookingController.verifyPaymentAndCreateBooking);
+
+// Restrict below routes for admin and leadGuides
+
+router.use(authController.restrictTo(ROLES.ADMIN, ROLES.GUIDE_LEADER));
+
+router
+  .route("/")
+  .get(bookingController.getAllBookings)
+  .post(bookingController.addNewBooking);
+
+router
+  .route("/:id")
+  .get(bookingController.getUniqueBooking)
+  .put(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
