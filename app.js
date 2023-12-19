d5de5390a3e03818be54560bad6bfef0e6552193
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-19 13:00:38 +0530

    feat: consume booking routes in app

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const CustomError = require("./utils/customError");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 const viewRouter = require("./routes/viewRoutes");
+const bookingRouter = require("./routes/bookingRoutes");
 
 const errorHandler = require("./controllers/errorController");
 
@@ -71,6 +72,7 @@ app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
 app.all("*", (req, res, next) => {
   next(
