e4b2ed136087bf0e98c7fce2465a3c51357dc646
AnkurChunekar <ankurchunekar@gmail.com>
2023-12-31 11:55:44 +0530

    feat: use compression middleware to compress responses

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
+const compression = require("compression");
 
 const tourRouter = require("./routes/tourRoutes");
 const CustomError = require("./utils/customError");
@@ -60,6 +61,9 @@ app.use(xss());
 
 // Prevent parameter pollution
 app.use(hpp());
+
+// COMPRESS RESPONSES
+app.use(compression());
 
 // CUSTOM EXAMPLE
 app.use((req, res, next) => {
